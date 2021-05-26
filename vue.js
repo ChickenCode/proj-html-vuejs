3250e29a0d27e7cd44d2fe545a1e32d41ad68173
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-26 09:53:33 -0400

    Commit pre styling

---
+++ vue.js
@@ -0,0 +1,66 @@
+new Vue({
+    el: "#root",
+    data: {
+        posts: [
+            {
+                title: "First random title"
+            },
+            {
+                title: "Another random title"
+            },
+            {
+                title: "Random titles still exist"
+            },
+            {
+                title: "10 best fruits to eat for breakfast"
+            },
+            {
+                title: "Eating bananas is dangerous for your health"
+            },
+            {
+                title: "The best protein shake"
+            },
+            {
+                title: "Tips to find training partners"
+            },
+            {
+                title: "New juices available now"
+            },
+            {
+                title: "Ultimate cardio workout"
+            },
+            {
+                title: "20 best healthy recipies"
+            },
+        ],
+
+        navbar: [
+            {
+                link: "Home",
+                add: ""
+            },
+            {
+                link: "Services",
+                add: ""
+            },
+            {
+                link: "About",
+                add: ""
+            },
+            {
+                link: "Videos",
+                add: ""
+            },
+            {
+                link: "Blog",
+                add: ""
+            },
+            {
+                link: "Store",
+                add: "new"
+            },
+        ]
+    },
+    
+    
+})
